56cb1b74471424726da94775d379782feb5649ec
Anant Tripathi <ianantktripathi@gmail.com>
2021-04-19 23:39:17 +0530

    My first commit

---
+++ src/main/java/com/anant/practice/daily/daily_coding_challenge/App.java
@@ -0,0 +1,13 @@
+package com.anant.practice.daily.daily_coding_challenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
