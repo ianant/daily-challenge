f658da8ffe5e41ef1048c281ac22895a2d4b24b8
Anant Tripathi <ianantktripathi@gmail.com>
2021-04-19 23:52:51 +0530

    Fist way change

--- src/main/java/com/anant/practice/daily/daily_coding_challenge/Day1/Day1SumOf2Numbers.java
+++ src/main/java/com/anant/practice/daily/daily_coding_challenge/Day1/Day1SumOf2Numbers.java
@@ -1,14 +1,14 @@
 
-
 package com.anant.practice.daily.daily_coding_challenge.Day1;
 
 import java.util.HashSet;
 
 /**
- * @author Anant
- * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
-
-	For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17
+ * @author Anant Given a list of numbers and a number k, return whether any two
+ *         numbers from the list add up to k.
+ * 
+ *         For example, given [10, 15, 3, 7] and k of 17, return true since 10 +
+ *         7 is 17
  *
  */
 public class Day1SumOf2Numbers {
@@ -16,11 +16,12 @@ public class Day1SumOf2Numbers {
 	public static void main(String[] args) {
 		int ar[]= {10, 15, 3, 7};
 		int k=17;
+		
 		/*
-	1:way	 * for(int i=0;i<ar.length-1;i++) { for(int j=i+1;j<ar.length;j++) {
+		 * 1:way for(int i=0;i<ar.length-1;i++) { for(int j=i+1;j<ar.length;j++) {
 		 * if(ar[i]+ar[j]==k) { System.out.println(ar[i]); System.out.println(ar[j]); }
 		 * else { //System.out.println("no number found"); } } }
-		 */
+		 */		 
 		
 		//2nd way:
 			HashSet<Integer> numberSet=new HashSet<Integer>();
